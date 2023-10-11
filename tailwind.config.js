7a516021c5e60fbb6ba8087607040ca14d042e58
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-10-11 21:47:49 +0530

    Added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,61 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			{
+				light: {
+					"primary": "#050538",
+					"secondary": "#4e3dae",
+					"accent": "#947a4c",
+					"neutral": "#27202d",
+					"base-100": "#ebe7ee",
+				},
+				dark: {
+					"primary": "#c7c7fa",
+					"secondary": "#6251c2",
+					"accent": "#b3986b",
+					"neutral": "#27202d",
+					"base-100": "#151118",
+				},
+			},
+			"light",
+			"dark",
+			"cupcake",
+			"bumblebee",
+			"emerald",
+			"corporate",
+			"synthwave",
+			"retro",
+			"cyberpunk",
+			"valentine",
+			"halloween",
+			"garden",
+			"forest",
+			"aqua",
+			"lofi",
+			"pastel",
+			"fantasy",
+			"wireframe",
+			"black",
+			"luxury",
+			"dracula",
+			"cmyk",
+			"autumn",
+			"business",
+			"acid",
+			"lemonade",
+			"night",
+			"coffee",
+			"winter",
+		],
+		darkTheme: "dark", // name of one of the included themes for dark mode
+		base: true, // applies background color and foreground color for root element by default
+		styled: true, // include daisyUI colors and design decisions for all components
+		utils: true, // adds responsive and modifier utility classes
+		rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+		prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+		logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+	},
 };
