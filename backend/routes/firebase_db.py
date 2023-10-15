4b879c89a294f8f1bb587b5b1bcd103ca73f9982
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-10-15 21:47:44 +0530

    emailing csv file with form data details works now

--- backend/routes/firebase_db.py
+++ backend/routes/firebase_db.py
@@ -17,6 +17,12 @@ from fastapi.exceptions import HTTPException
 # Importing Firebase stuff.
 from firebase_admin import auth, db
 
+# importing pandas
+import pandas as pd
+
+# Importing email functionality
+from utilities.send_email import send_csv_file_of_form_data
+
 
 ref = db.reference( "/", url = "https://there-all-along-default-rtdb.asia-southeast1.firebasedatabase.app/" )
 
@@ -32,11 +38,10 @@ class CreateFormInfo( BaseModel ) :
     token: str
     form_attributes: List[ str ]
 
-class AddFormData(BaseModel) :
+class AddFormData( BaseModel ) :
     token: str
     form_id: str
     form_data: dict
-
 
 # get forms endpoint
 @router.get( "/get_forms" )
@@ -173,230 +178,300 @@ async def update_form( updateFormInfo: CreateFormInfo ) :
         )
 
 # delete form endpoint
-@router.delete("/delete_form")
-async def delete_form(deleteFormInfo: UserFormInfo):
+@router.delete( "/delete_form" )
+async def delete_form( deleteFormInfo: UserFormInfo ) :
     token = deleteFormInfo.token
     form_id = deleteFormInfo.form_id
-    try:
-        user = auth.verify_id_token(token)
-        uid = user["uid"]
-        forms_ref = ref.child("users").child(uid).child("forms")
-        form_ref = forms_ref.child(form_id)
+    try :
+        user = auth.verify_id_token( token )
+        uid = user[ "uid" ]
+        forms_ref = ref.child( "users" ).child( uid ).child( "forms" )
+        form_ref = forms_ref.child( form_id )
         
         # Check if the form exists
-        if not form_ref.get():
+        if not form_ref.get() :
             raise HTTPException(
-                    detail={"message": "Form not found"},
-                    status_code=404
+                    detail = { "message" : "Form not found" },
+                    status_code = 404
             )
         
         # Perform the deletion using .delete()
-        forms_ref.child(form_id).delete()
+        forms_ref.child( form_id ).delete()
         
-        return {"message": "Form deleted successfully"}
-    except UserNotFoundError as e:
-        print(e)
+        return { "message" : "Form deleted successfully" }
+    except UserNotFoundError as e :
+        print( e )
         return HTTPException(
-                detail={"message": "Error! User not found."},
-                status_code=400
+                detail = { "message" : "Error! User not found." },
+                status_code = 400
         )
-    except Exception as e:
-        print(e)
+    except Exception as e :
+        print( e )
         return HTTPException(
-                detail={"message": "Error in deleting form."},
-                status_code=500
+                detail = { "message" : "Error in deleting form." },
+                status_code = 500
         )
 
 # add form data endpoint
-@router.post("/add_form_data")
-async def add_form_data(addFormDataInfo: AddFormData):
+@router.post( "/add_form_data" )
+async def add_form_data( addFormDataInfo: AddFormData ) :
     token = addFormDataInfo.token
     form_id = addFormDataInfo.form_id
     form_data = addFormDataInfo.form_data
-    try:
-        user = auth.verify_id_token(token)
-        uid = user["uid"]
-        forms_ref = ref.child("users").child(uid).child("forms")
-        form_ref = forms_ref.child(form_id)
+    try :
+        user = auth.verify_id_token( token )
+        uid = user[ "uid" ]
+        forms_ref = ref.child( "users" ).child( uid ).child( "forms" )
+        form_ref = forms_ref.child( form_id )
         
         # Check if the form exists
-        if not form_ref.get():
+        if not form_ref.get() :
             raise HTTPException(
-                    detail={"message": "Form not found"},
-                    status_code=404
+                    detail = { "message" : "Form not found" },
+                    status_code = 404
             )
         
         # Check if keys in form_data match form attributes
-        form_attributes = form_ref.child("attributes").get()
-        if set(form_data.keys()) == set(form_attributes):
+        form_attributes = form_ref.child( "attributes" ).get()
+        if set( form_data.keys() ) == set( form_attributes ) :
             # Push form data to the "data" object of the form
-            form_ref.child("data").push(form_data)
-            return {"message": "Form data added successfully"}
-        else:
+            form_ref.child( "data" ).push( form_data )
+            # get key of the latest data added.
+            # This is the id of the form data
+            form_data_id = form_ref.child( "data" ).get()
+            form_data_id = list( form_data_id.keys() )[ -1 ]
+            returning_dict = {
+                "message" : "Form data added successfully",
+                "form_data_id" : form_data_id
+            }
+            return returning_dict
+        else :
             raise HTTPException(
-                    detail={"message": "Keys in form data do not match form attributes"},
-                    status_code=400
+                    detail = { "message" : "Keys in form data do not match form attributes" },
+                    status_code = 400
             )
     
-    except UserNotFoundError as e:
-        print(e)
+    except UserNotFoundError as e :
+        print( e )
         return HTTPException(
-                detail={"message": "Error! User not found."},
-                status_code=400
+                detail = { "message" : "Error! User not found." },
+                status_code = 400
         )
-    except Exception as e:
-        print(e)
+    except Exception as e :
+        print( e )
         return HTTPException(
-                detail={"message": "Error in adding form data."},
-                status_code=500
+                detail = { "message" : "Error in adding form data." },
+                status_code = 500
         )
 
 # update form data endpoint
-class UpdateFormData(BaseModel):
-    token : str
-    form_id : str
-    form_data_id : str
-    updated_data : dict
+class UpdateFormData( BaseModel ) :
+    token: str
+    form_id: str
+    form_data_id: str
+    updated_data: dict
 
-@router.put("/update_form_data")
-async def update_form_data(updateFormDataInfo: UpdateFormData):
+@router.put( "/update_form_data" )
+async def update_form_data( updateFormDataInfo: UpdateFormData ) :
     token = updateFormDataInfo.token
     form_id = updateFormDataInfo.form_id
     form_data_id = updateFormDataInfo.form_data_id
     updated_data = updateFormDataInfo.updated_data
     
-    try:
-        user = auth.verify_id_token(token)
-        uid = user["uid"]
-        forms_ref = ref.child("users").child(uid).child("forms")
-        form_ref = forms_ref.child(form_id).child("data").child(form_data_id)
+    try :
+        user = auth.verify_id_token( token )
+        uid = user[ "uid" ]
+        forms_ref = ref.child( "users" ).child( uid ).child( "forms" )
+        form_ref = forms_ref.child( form_id ).child( "data" ).child( form_data_id )
         
         # Check if the form data exists
-        if not form_ref.get():
+        if not form_ref.get() :
             raise HTTPException(
-                    detail={"message": "Form data not found"},
-                    status_code=404
+                    detail = { "message" : "Form data not found" },
+                    status_code = 404
             )
         
         # Update the form data
-        form_ref.update(updated_data)
-        return {"message": "Form data updated successfully"}
+        form_ref.update( updated_data )
+        return { "message" : "Form data updated successfully" }
     
-    except UserNotFoundError as e:
-        print(e)
+    except UserNotFoundError as e :
+        print( e )
         return HTTPException(
-                detail={"message": "Error! User not found."},
-                status_code=400
+                detail = { "message" : "Error! User not found." },
+                status_code = 400
         )
-    except Exception as e:
-        print(e)
+    except Exception as e :
+        print( e )
         return HTTPException(
-                detail={"message": "Error in updating form data."},
-                status_code=500
+                detail = { "message" : "Error in updating form data." },
+                status_code = 500
         )
 
 # delete form data endpoint
-class DeleteFormData(BaseModel) :
-    token : str
-    form_id : str
-    form_data_id : str
+class DeleteFormData( BaseModel ) :
+    token: str
+    form_id: str
+    form_data_id: str
 
-@router.delete("/delete_form_data")
-async def delete_form_data(deleteFormDataInfo: DeleteFormData):
+@router.delete( "/delete_form_data" )
+async def delete_form_data( deleteFormDataInfo: DeleteFormData ) :
     token = deleteFormDataInfo.token
     form_id = deleteFormDataInfo.form_id
     form_data_id = deleteFormDataInfo.form_data_id
     
-    try:
-        user = auth.verify_id_token(token)
-        uid = user["uid"]
-        forms_ref = ref.child("users").child(uid).child("forms")
-        form_ref = forms_ref.child(form_id).child("data").child(form_data_id)
+    try :
+        user = auth.verify_id_token( token )
+        uid = user[ "uid" ]
+        forms_ref = ref.child( "users" ).child( uid ).child( "forms" )
+        form_ref = forms_ref.child( form_id ).child( "data" ).child( form_data_id )
         
         # Check if the form data exists
-        if not form_ref.get():
+        if not form_ref.get() :
             raise HTTPException(
-                    detail={"message": "Form data not found"},
-                    status_code=404
+                    detail = { "message" : "Form data not found" },
+                    status_code = 404
             )
         
         # Delete the form data
         form_ref.delete()
-        return {"message": "Form data deleted successfully"}
+        return { "message" : "Form data deleted successfully" }
     
-    except UserNotFoundError as e:
-        print(e)
+    except UserNotFoundError as e :
+        print( e )
         return HTTPException(
-                detail={"message": "Error! User not found."},
-                status_code=400
+                detail = { "message" : "Error! User not found." },
+                status_code = 400
         )
-    except Exception as e:
-        print(e)
+    except Exception as e :
+        print( e )
         return HTTPException(
-                detail={"message": "Error in deleting form data."},
-                status_code=500
+                detail = { "message" : "Error in deleting form data." },
+                status_code = 500
         )
 
 # get all form data endpoint
-class GetAllFormData(BaseModel):
-    token : str
-    form_id : str
+class GetAllFormData( BaseModel ) :
+    token: str
+    form_id: str
 
-@router.get("/get_all_form_data")
-async def get_all_form_data(getAllFormDataInfo: GetAllFormData):
+@router.get( "/get_all_form_data" )
+async def get_all_form_data( getAllFormDataInfo: GetAllFormData ) :
     token = getAllFormDataInfo.token
     form_id = getAllFormDataInfo.form_id
     
-    try:
-        user = auth.verify_id_token(token)
-        uid = user["uid"]
-        forms_ref = ref.child("users").child(uid).child("forms")
-        form_data_ref = forms_ref.child(form_id).child("data").get()
+    try :
+        user = auth.verify_id_token( token )
+        uid = user[ "uid" ]
+        forms_ref = ref.child( "users" ).child( uid ).child( "forms" )
+        form_data_ref = forms_ref.child( form_id ).child( "data" ).get()
         
         return form_data_ref
     
-    except UserNotFoundError as e:
-        print(e)
+    except UserNotFoundError as e :
+        print( e )
         return HTTPException(
-                detail={"message": "Error! User not found."},
-                status_code=400
+                detail = { "message" : "Error! User not found." },
+                status_code = 400
         )
-    except Exception as e:
-        print(e)
+    except Exception as e :
+        print( e )
         return HTTPException(
-                detail={"message": "Error in getting form data."},
-                status_code=500
+                detail = { "message" : "Error in getting form data." },
+                status_code = 500
         )
 
 # get single form data endpoint
-class GetSingleFormData(BaseModel) :
-    token : str
-    form_id : str
-    form_data_id : str
+class GetSingleFormData( BaseModel ) :
+    token: str
+    form_id: str
+    form_data_id: str
 
-@router.get("/get_single_form_data")
-async def get_single_form_data(getSingleFormDataInfo: GetSingleFormData):
+@router.get( "/get_single_form_data" )
+async def get_single_form_data( getSingleFormDataInfo: GetSingleFormData ) :
     token = getSingleFormDataInfo.token
     form_id = getSingleFormDataInfo.form_id
     form_data_id = getSingleFormDataInfo.form_data_id
     
-    try:
-        user = auth.verify_id_token(token)
-        uid = user["uid"]
-        forms_ref = ref.child("users").child(uid).child("forms")
-        form_data_ref = forms_ref.child(form_id).child("data").child(form_data_id).get()
+    try :
+        user = auth.verify_id_token( token )
+        uid = user[ "uid" ]
+        forms_ref = ref.child( "users" ).child( uid ).child( "forms" )
+        form_data_ref = forms_ref.child( form_id ).child( "data" ).child( form_data_id ).get()
         
         return form_data_ref
     
-    except UserNotFoundError as e:
-        print(e)
+    except UserNotFoundError as e :
+        print( e )
         return HTTPException(
-                detail={"message": "Error! User not found."},
-                status_code=400
+                detail = { "message" : "Error! User not found." },
+                status_code = 400
         )
-    except Exception as e:
-        print(e)
+    except Exception as e :
+        print( e )
         return HTTPException(
-                detail={"message": "Error in getting form data."},
-                status_code=500
+                detail = { "message" : "Error in getting form data." },
+                status_code = 500
+        )
+
+# email csv file of the form data endpoint
+class EmailCSVFile( BaseModel ) :
+    token: str
+    form_id: str
+    email: str
+
+def convert_form_data_to_csv( form_data_ref ) :
+    # use pandas to create the csv file
+    df = pd.DataFrame.from_dict( form_data_ref, orient = 'index' )
+    # convert this into a string that can be used in a csv
+    csv_file = ""
+    # headers
+    for i in df :
+        csv_file += i + ","
+    csv_file += "\n"
+    for i in range( len( df ) ) :
+        val = df.iloc[ i ].values
+        for j in val :
+            csv_file += j + ","
+        csv_file += "\n"
+    return csv_file
+
+@router.post( "/email_csv_file_form_data" )
+async def email_csv_file_form_data( emailCSVFileInfo: EmailCSVFile ) :
+    token = emailCSVFileInfo.token
+    form_id = emailCSVFileInfo.form_id
+    email = emailCSVFileInfo.email
+    
+    try :
+        user = auth.verify_id_token( token )
+        uid = user[ "uid" ]
+        forms_ref = ref.child( "users" ).child( uid ).child( "forms" )
+        form_data_ref = forms_ref.child( form_id ).child( "data" ).get()
+        
+        # Check if the form data exists
+        if not form_data_ref :
+            raise HTTPException(
+                    detail = { "message" : "Form data not found" },
+                    status_code = 404
+            )
+        
+        # Convert the form data to a csv file
+        csv_file = convert_form_data_to_csv( form_data_ref )
+        
+        # Send the csv file to the email
+        send_csv_file_of_form_data( email, "Your Requested Form Data", csv_file )
+        
+        return { "message" : "CSV file sent to the email" }
+    
+    except UserNotFoundError as e :
+        print( e )
+        return HTTPException(
+                detail = { "message" : "Error! User not found." },
+                status_code = 400
+        )
+    except Exception as e :
+        print( e )
+        return HTTPException(
+                detail = { "message" : "Error in sending csv file." },
+                status_code = 500
         )
--- backend/utilities/send_email.py
+++ backend/utilities/send_email.py
@@ -4,13 +4,16 @@
 import os
 import smtplib
 from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 
 # getting the login and password from the environment variables on this pc
 
 EMAIL_ADDRESS = "puzzlelists@gmail.com"
 with open("private/email_pass.txt", "r") as f:
     EMAIL_PASSWORD = f.read()
-    
+
 
 def send_reset_mail( To, Subject= "Reset Password Link", link = "" ):
     """
@@ -67,6 +70,33 @@ def send_verification_mail( To, Subject= "Verify Your Email with There All Along
     msg.set_content(content)
     
     # sending the email, by creating an SMTP_SSL instance
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+            smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+            smtp.send_message(msg)
+        return 1
+    except:
+        return 0
+
+def send_csv_file_of_form_data(To, Subject, csv_content):
+    EMAIL_ADDRESS = "puzzlelists@gmail.com"  # Replace with your Gmail address
+    
+    # Create a message instance
+    msg = MIMEMultipart()
+    msg["Subject"] = Subject
+    msg["From"] = EMAIL_ADDRESS
+    msg["To"] = To
+    
+    # Add plain text content to the email
+    content = f"Here is the CSV File of the Form Data that you requested. \n\nRegards\nThere All Along\nKrishnaraj"
+    msg.attach(MIMEText(content, 'plain'))
+    
+    # Attach the CSV content as a CSV file
+    csv_attachment = MIMEText(csv_content, 'csv')
+    csv_attachment.add_header('Content-Disposition', 'attachment', filename="data.csv")
+    msg.attach(csv_attachment)
+    
+    # Sending the email by creating an SMTP_SSL instance
     try:
         with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
             smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
